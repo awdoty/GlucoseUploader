82c8ab61c1b01c6394cede0cd318f6e9bab5680a
Austin Doty <awdoty@icloud.com>
2025-03-14 18:24:19 -0700

    Auto-update: 2025-03-13 05:54:26

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,12 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.android.application") version "8.9.0" apply false
+    id("com.android.library") version "8.9.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
